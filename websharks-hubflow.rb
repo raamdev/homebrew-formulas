ce609c4f5cb356c4eb9251d2035215af8c8695af
jaswsinc <jas@wsharks.com>
2015-01-31 10:08:40 -0900

    Adding formulas.

---
+++ websharks-hubflow.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class WebSharksHubflow < Formula
+  version "140811"
+  homepage "http://github.com/websharks/hubflow"
+  url "https://github.com/websharks/hubflow.git"
+  sha1 "4bde2d619f00741b34714b3be74ae21af79d648c"
+
+  def install
+    ENV['INSTALL_INTO'] = libexec
+    system "./install.sh", "install"
+    bin.write_exec_script libexec/'git-hf'
+  end
+
+  test do
+    system bin/"git-hf", "version"
+  end
+end
---
+++ websharks-osa.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class WebSharksOsa < Formula
+  version "150131"
+  homepage "https://github.com/websharks/osa"
+  url "https://github.com/websharks/osa.git"
+  sha1 "1e45a118bfae9a6988a87bc2a08de275131ce3a9"
+
+  def install
+    script_libraries = File.expand_path("~/Library/Script Libraries")
+    ln_s "#{prefix}", "#{script_libraries}/websharks", :force => true
+  end
+
+  test do
+  end
+end
