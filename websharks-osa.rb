854e50cc186188a9228ddee3ed691c005806ad2c
jaswsinc <jas@wsharks.com>
2015-02-01 17:08:17 -0900

    Create Typinator symlinks.

--- websharks-osa.rb
+++ websharks-osa.rb
@@ -7,13 +7,24 @@ class WebsharksOsa < Formula
   sha1 "5449db480c6154d69e8614f66fe06525fbd2b7b4"
 
   def install
-    prefix.install Dir["*"]
-    script_libraries = File.expand_path("~/Library/Script Libraries")
+    prefix.install Dir["*"] # Install files.
 
-    if !File.directory? "#{script_libraries}"
-      mkdir "#{script_libraries}"
+    script_libraries_dir = File.expand_path("~/Library/Script Libraries")
+    typinator_scripts_dir1 = File.expand_path("~/Typinator/Includes/Scripts")
+    typinator_scripts_dir2 = File.expand_path("~/Library/Application Support/Typinator/Sets/Includes/Scripts")
+
+    if !File.directory? "#{script_libraries_dir}"
+      mkdir "#{script_libraries_dir}"
+    end # Make this directory if it does not exist yet.
+    
+    ln_s "#{prefix}", "#{script_libraries_dir}/websharks", :force => true
+
+    if File.directory? "#{typinator_scripts_dir1}"
+      ln_s "#{prefix}/scripts/typinator.bash", "#{typinator_scripts_dir1}/runOSA", :force => true
     end
-    ln_s "#{prefix}", "#{script_libraries}/websharks", :force => true
+    if File.directory? "#{typinator_scripts_dir2}"
+      ln_s "#{prefix}/scripts/typinator.bash", "#{typinator_scripts_dir2}/runOSA", :force => true
+    end
   end
 
   test do
