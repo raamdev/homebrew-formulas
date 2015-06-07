cda45fc203a6e480e0cb9b10d42ee99f53c34ac0
jaswsinc <jas@wsharks.com>
2015-06-06 20:57:55 -0800

    Updating CLI tools formula.

--- Formula/websharks-cli-tools.rb
+++ Formula/websharks-cli-tools.rb
@@ -1,18 +1,26 @@
 require "formula"
 require File.expand_path("../../Requirements/Php/websharks-php-version-requirement", __FILE__)
+require File.expand_path("../../Requirements/Php/websharks-php-phar-requirement", __FILE__)
 
 class WebsharksCliTools < Formula
   homepage "https://github.com/websharks/cli-tools"
-  head "https://github.com/websharks/cli-tools.git", :branch => "000000-dev"
-  url "https://github.com/websharks/cli-tools.git", :tag => "000000-dev"
+  url "https://github.com/websharks/cli-tools/releases/download/150606/cli-tools.phar"
+  version "150606"
 
   depends_on WebsharksPhpVersionRequirement
-  depends_on "terminal-notifier" => :recommended
+  depends_on WebsharksPhpPharRequirement
+  depends_on "gnuplot" => :recommended
 
   def install
-    prefix.install Dir["*"]
+    mv "cli-tools.phar", "cli-tools-#{version}.phar"
+    libexec.install "cli-tools-#{version}.phar"
+    sh = libexec + "ws"
+    sh.write("#!/usr/bin/env bash\n\n/usr/bin/env php -d ___cli.phar_proxy=\"$0\" -d allow_url_fopen=On -d detect_unicode=Off -d date.timezone=UTC #{libexec}/websharks-cli-tools-#{version}.phar \"$@\"")
+    chmod 0755, sh
+    bin.install_symlink sh
   end
 
   test do
+    `system "ws", "--version"`
   end
 end
