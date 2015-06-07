aeba2f3980fe3ce1ca56cc0dd779a7252765fa1e
jaswsinc <jas@wsharks.com>
2015-06-06 21:38:05 -0800

    Updating CLI tools sha.

--- Formula/websharks-cli-tools.rb
+++ Formula/websharks-cli-tools.rb
@@ -5,7 +5,7 @@ require File.expand_path("../../Requirements/Php/websharks-php-phar-requirement"
 class WebsharksCliTools < Formula
   homepage "https://github.com/websharks/cli-tools"
   url "https://github.com/websharks/cli-tools/releases/download/150606/cli-tools.phar"
-  sha256 "512e9fa2ecd024c6e3c325f47698ba37dc925a456f5d8e43641319bfaeaf2d38"
+  sha256 "05f2054b3be58cd90a7ce9c89fe277f03fc21647029277183ec5e14a8534741c"
   version "150606"
 
   depends_on WebsharksPhpVersionRequirement
